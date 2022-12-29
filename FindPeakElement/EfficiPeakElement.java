89efb19c8c36a126528bfedb05d921235170055c
Elija33 <amponsaeli@gmail.com>
2022-12-29 03:19:21 -0500

    Find Peak element

---
+++ FindPeakElement/EfficiPeakElement.java
@@ -0,0 +1,22 @@
+package FindPeakElement;
+
+public class EfficiPeakElement {
+    public static int PeakEle(int arr[]){
+        int start = 0;
+        int end = arr.length;
+        while(start <= end){
+            int mid = (start + end) / 2;
+            if((mid == 0 || arr[mid] >= arr[mid - 1]) && (mid == arr.length - 1 || arr[mid] >= arr[mid + 1]))
+                return mid;
+            if(mid > 0 && arr[mid - 1] >= arr[mid])
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int arr[] = {5, 20, 40, 30, 20, 50, 60};
+        System.out.println(PeakEle(arr));
+    }
+}
---
+++ FindPeakElement/PeakElement.java
@@ -0,0 +1,16 @@
+package FindPeakElement;
+
+public class PeakElement {
+    public static int PeakElement(int arr[]){
+        for(int i = 1; i < arr.length-1; i++){
+            if(arr[i] > arr[i + 1] && arr[i] >= arr[i - 1]);
+                 return arr[i];
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {5, 10, 20, 15, 7};
+        System.out.println(PeakElement(arr));
+    }
+}
